6161eee273f7eaaf31b46ca29aaa46d17941fb69
de9vdara <150059423+de9vdara@users.noreply.github.com>
2025-07-08 20:17:00 +0400

    feat: testing husky

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
+    
     console.log('Hello from AppService!');
     
     return 'Hello World!';
